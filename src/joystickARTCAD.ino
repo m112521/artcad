9636174717b0bb3327140b049922b492dd5170cb
Azamat Tavitov <aztapps@gmail.com>
2023-10-18 21:16:16 +0300

    add relay code

--- src/joystickARTCAD.ino
+++ src/joystickARTCAD.ino
@@ -7,18 +7,14 @@ int data[5];
 
 const int xPin = A0;
 const int yPin = A1;
-const int buttonPin = 4;
 const int ptmrPin = A2;
 const int tumblrPin = 6;
 
 int xPosition = 0;
 int yPosition = 0;
-int buttonState = 0;
 int tumblrState = 0;
 int ptmrState = 0;
 
-int speed = 0;
-int direction = 0;
                                 
 void setup(){
     radio.begin();
@@ -26,19 +22,21 @@ void setup(){
     radio.setDataRate     (RF24_1MBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
     radio.setPALevel      (RF24_PA_HIGH);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
     radio.openWritingPipe (0x1234567890LL);               // Открываем трубу с идентификатором 0x1234567890 для передачи данных (на одном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
-    //radio.stopListening();
     pinMode(ptmrPin, INPUT);
 }
 void loop(){
     ptmrState = analogRead(ptmrPin);
+    tumblrState = digitalRead(tumblrPin);
+    xPosition = analogRead(yPin);
+    yPosition = analogRead(xPin);
     
-    data[0] = 0;
-    data[1] = 0;
-    data[2] = 0;
-    data[3] = 0;
+    data[0] = 0; // dummy value. Instead you can send some meaningful value - button state, for example
+    data[1] = xPosition;
+    data[2] = yPosition;
+    data[3] = tumblrState;
     data[4] = ptmrState;
-    radio.write(&data, sizeof(data));                     // указывая сколько байт массива мы хотим отправить. с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){ приняты;}else{не приняты приёмником;}
+    radio.write(&data, sizeof(data));  // указывая сколько байт массива мы хотим отправить. с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){ приняты;}else{не приняты приёмником;}
 
-    //Serial.println(ptmrState);
-    //delay(100);
+    // Serial.println(yPosition);
+    // delay(100);
 }
--- src/robotARTCAD.ino
+++ src/robotARTCAD.ino
@@ -8,12 +8,20 @@
 #define SPEED_2      6
 #define DIR_2        7
 
-#define MATRIX_PIN    0
-#define LED_COUNT 16
+#define RELAY_PIN 3
 
 
 RF24 radio(8, 9); // nRF24L01+ (CE, CSN)
-int data[5]; 
+int data[5];  // создаем массив для приема данных по радиоканалу
+
+// Создаем переменные для хранения значений с джойстика, тумблера и потенциометра
+int xPosition = 0; 
+int yPosition = 0;
+int tumblrState = 0;
+int ptmrVal = 0;
+
+// Переменная для храения значения скорости вращения моторов
+int speed = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -24,26 +32,69 @@ void setup(){
   radio.setPALevel      (RF24_PA_HIGH);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
   radio.openReadingPipe (1, 0x1234567890LL);            // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на одном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
   radio.startListening  ();                             // Включаем приемник, начинаем прослушивать открытую трубу
-  //  radio.stopListening   ();                         // Выключаем приёмник, если потребуется передать данные
 
-  // motor pins
+  // Устанавливаем режим работы пинов 
   for (int i = 4; i < 8; i++) {     
     pinMode(i, OUTPUT);
   }
+  pinMode(RELAY_PIN, INPUT);
 }
 
 void loop(){
-    if(radio.available()){                                
+    // Если есть соединение с джойстиком, то ...
+    if(radio.available()){   
+        // ..ю то считываем из радиоканала значения с джойстика, тумблера и потенциометра - они хранятся в массиве data                             
         radio.read(&data, sizeof(data));             
 
-        int ptmrVal = data[4];
-        int speed = map(ptmrVal, 0, 1023, 0, 255); 
-        Serial.println(ptmrVal);
+        // Сохраняем значения из массива в переменные (чтобы не запутаться с индексами массива)
+        xPosition = data[1];
+        yPosition = data[2];
+        tumblrState = data[3];
+        ptmrVal = data[4];
+        speed = map(ptmrVal, 0, 1023, 0, 255); // приводим значения с потенциометра к диапазаону [0,255], так как 255 - макс значение скорости для мотора 
 
-        digitalWrite(DIR_1, HIGH); 
-        analogWrite(SPEED_1, speed); 
+        // Если значение на тумблере 1, то включаем реле и запускаем мотор орудия
+        if (tumblrState) {
+          digitalWrite(RELAY_PIN, HIGH);
+        }
+        else { // Если значение на тумблере 0, то выключаем реле и мотор
+          digitalWrite(RELAY_PIN, LOW);
+        }
 
-        digitalWrite(DIR_2, HIGH); 
-        analogWrite(SPEED_2, speed);      
+        // Управление направлением вращения колес в зависимости от положения джойстика на пульте управления
+        if (xPosition > 550) {
+          digitalWrite(DIR_1, HIGH); // направление мотора 1 - вперед
+          analogWrite(SPEED_1, speed); // скорость speed зависит от значения на потенциометре
+
+          digitalWrite(DIR_2, HIGH); // направление мотора 2 - вперед
+          analogWrite(SPEED_2, speed);   
+        }
+        else if (xPosition < 500) {
+          digitalWrite(DIR_1, LOW);  // направление мотора 1 - назад
+          analogWrite(SPEED_1, speed); 
+
+          digitalWrite(DIR_2, LOW); // направление мотора 2 - назад
+          analogWrite(SPEED_2, speed);           
+        }
+        else if (yPosition > 700) {
+          digitalWrite(DIR_1, LOW); // направление мотора 1 - назад
+          analogWrite(SPEED_1, speed); 
+
+          digitalWrite(DIR_2, HIGH); // направление мотора 2 - вперед
+          analogWrite(SPEED_2, speed); 
+        }
+        else if (yPosition < 300) {
+          digitalWrite(DIR_1, HIGH); // направление мотора 1 - вперед
+          analogWrite(SPEED_1, speed); 
+
+          digitalWrite(DIR_2, LOW); // направление мотора 2 - назад
+          analogWrite(SPEED_2, speed); 
+        }             
+        else { // останавливаем моторы
+          analogWrite(SPEED_1, 0); 
+          analogWrite(SPEED_2, 0);           
+        }
+      
+        //Serial.println(tumblrState);
     }
 }
